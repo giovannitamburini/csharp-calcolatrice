e871e2d7ffa9a2ba4f68082d564783cd744367b4
giovannitamburini <giovatambu@gmail.com>
2023-09-12 16:45:50 +0200

    completed exercise with bonus

--- csharp-calcolatrice/CalculationsHelper.cs
+++ csharp-calcolatrice/CalculationsHelper.cs
@@ -30,75 +30,118 @@ namespace csharp_calcolatrice
              */
 
         // funzione che somma due numeri interi
-        public static int CalculateSumOfTwoInt(int num1, int num2)
+        public static int CalculateSumOfTwoNum(int num1, int num2)
         {
             return num1 + num2;
         }
 
         // funzione che somma due numeri double
-        public static double CalculateSumOfTwoDouble(double num1, double num2)
+        public static double CalculateSumOfTwoNum(double num1, double num2)
         {
-            return num1 + num2;
+            return Math.Round(num1 + num2, 2);
         }
 
         // funzione che fa la sottrazione tra due interi
-        public static int CalculateDifferenceOfTwoInt(int num1, int num2)
+        public static int CalculateDifferenceOfTwoNum(int num1, int num2)
         {
             return num1 - num2;
         }
 
         // funzione che fa la differenza tra due double
-        public static double CalculateDifferenceOfTwoDouble(double num1, double num2)
+        public static double CalculateDifferenceOfTwoNum(double num1, double num2)
         {
-            return num1 - num2;
+            return Math.Round(num1 - num2, 2);
         }
 
         // funzione per moltiplicare tra loro due interi
-        public static int CalculateProductBetweenIntegers(int num1, int num2)
+        public static int CalculateProductBetweenTwoNum(int num1, int num2)
         {
             return num1 * num2;
         }
 
         // funzione per moltiplicare tra loro due double
-        public static double CalculateProductBetweenDoubles(double num1, double num2)
+        public static double CalculateProductBetweenTwoNum(double num1, double num2)
         {
-            return num1 * num2;
+            return Math.Round(num1 * num2, 2);
         }
 
         // funzione per calcolare il valore assoluto di un intero
-        public static int CalculateIntAbsoluteValue(int num)
+        public static int CalculateAbsoluteValue(int num)
         {
             return Math.Abs(num);
         }
 
         // funzione per calcolare il valore assoluto di un double
-        public static double CalculateDoubleAbsoluteValue(double num)
+        public static double CalculateAbsoluteValue(double num)
         {
             return Math.Abs(num);
         }
 
         // funzione per controllare il minimo tra due int
-        public static int CheckMinBetweenTwoInt(int num1, int num2)
+        public static int CheckMinBetweenTwoNum(int num1, int num2)
         {
             return Math.Min(num1, num2);
         }
 
         // funzione per controllare il minimo tra due double
-        public static double CheckMinBetweenTwoDouble(double num1, double num2)
+        public static double CheckMinBetweenTwoNum(double num1, double num2)
         {
             return Math.Min(num1, num2);
         }
 
         // funzione per controllare il massimo tra due int
-        public static int CheckMaxBetweenTwoInt(int num1, int num2)
+        public static int CheckMaxBetweenTwoNum(int num1, int num2)
         {
             return Math.Max(num1, num2);
         }
 
         // funzione per controllare il massimo tra due double
-        public static double CheckMaxBetweenTwoDouble(double num1, double num2)
+        public static double CheckMaxBetweenTwoNum(double num1, double num2)
         {
             return Math.Max(num1, num2);
+        }
+
+        // BONUS
+        // Aggiungete il metodo per l’elevamento a potenza che prende come parametri due numeri interi (base ed esponente) e ritorna la base elevata all’esponente.
+        // Occhio che sia la base che l’esponente potrebbero essere uguali a zero oppure numeri negativi! (caso esponente = 0 e base = 0 anche se in matematica sarebbe un numero indefinito usiamo l’approccio informatico che lo calcola come 1)
+
+        public static double CalculateExponentiation(int numBase, int exponent)
+        {
+            // controllo che numBase non sia uguale a 0
+            if (numBase == 0)
+            {
+                numBase = 1;
+            }
+
+            double Exponential = numBase;
+
+            // controllo che l'esponente non sia uguale a 0
+            if (exponent == 0)
+            {
+                exponent = 1;
+
+                return Exponential;
+
+            } else if (exponent < 0)
+            {
+                exponent *= -1;
+
+                for (int i = 0; i < exponent - 1; i++)
+                {
+                    Exponential *= numBase;
+                }
+
+                return 1 / Exponential;
+
+            } else
+            {
+                for (int i = 0; i < exponent - 1; i++)
+                {
+                    Exponential *= numBase;
+                }
+
+                return Exponential;
+            }
         }
     }
 }
--- csharp-calcolatrice/Program.cs
+++ csharp-calcolatrice/Program.cs
@@ -9,21 +9,81 @@
             repo : csharp-calcolatrice
             Creare una classe di helper CalcoliHelper.
             Come visto a lezione, strutturare la classe in modo che non possa essere istanziata e che presenti i seguenti metodi static:
-            Somma di due numeri interi
-            Somma di due numeri double
-            Differenza tra due numeri interi
-            Differenza tra due numeri double
-            Moltiplicazione di due numeri interi
-            Moltiplicazione di due numeri double
-            Valore assoluto di un numero intero
-            Valore assoluto di un numero double
-            Minimo tra due numeri interi
-            Minimo tra due numeri double
-            Massimo tra due numeri interi
-            Massimo tra due numeri double
+            - Somma di due numeri interi
+            - Somma di due numeri double
+            - Differenza tra due numeri interi
+            - Differenza tra due numeri double
+            - Moltiplicazione di due numeri interi
+            - Moltiplicazione di due numeri double
+            - Valore assoluto di un numero intero
+            - Valore assoluto di un numero double
+            - Minimo tra due numeri interi
+            - Minimo tra due numeri double
+            - Massimo tra due numeri interi
+            - Massimo tra due numeri double
             Il fatto di dover scrivere lo stesso metodo per tipi di parametro diversi applica nella pratica uno dei principi di programmazione ad oggetti che abbiamo visto...Quale?
             Scrivetelo in un commento all’interno del codice! :faccia_nerd: Testate tutti i metodi della vostra classe di helper (con un esempio per ogni casistica).
-             */
+            */
+            int IntNum1 = 5;
+            int IntNum2 = 9;
+
+            int IntNum3 = -5;
+
+            double DoubleNum1 = 3.5d;
+            double DoubleNum2 = 8.3d;
+
+            double DoubleNum3 = -7.8d;
+
+            // controllo le funzioni per la somma
+            int IntSum = CalculationsHelper.CalculateSumOfTwoNum(IntNum1, IntNum2);
+            Console.WriteLine($"La somma tra {IntNum1} e {IntNum2} é: {IntSum}");
+
+            double DoubleSum = CalculationsHelper.CalculateSumOfTwoNum(DoubleNum1, DoubleNum2);
+            Console.WriteLine($"La somma tra {DoubleNum1} e {DoubleNum2} é: {DoubleSum}");
+
+            // controllo le funzioni per la differenza
+            int IntDiff = CalculationsHelper.CalculateDifferenceOfTwoNum(IntNum1, IntNum2);
+            Console.WriteLine($"La diffrenza tra {IntNum1} e {IntNum2} é: {IntDiff}");
+
+            double DoubleDiff = CalculationsHelper.CalculateDifferenceOfTwoNum(DoubleNum1, DoubleNum2);
+            Console.WriteLine($"La differenza tra {DoubleNum1} e {DoubleNum2} é: {DoubleDiff}");
+
+            // controllo le funzioni per la moltiplicazione
+            int IntProduct = CalculationsHelper.CalculateProductBetweenTwoNum(IntNum1, IntNum2);
+            Console.WriteLine($"Il prodotto tra {IntNum1} e {IntNum2} é: {IntProduct}");
+
+            double DoubleProduct = CalculationsHelper.CalculateProductBetweenTwoNum(DoubleNum1, DoubleNum2);
+            Console.WriteLine($"Il prodotto tra {DoubleNum1} e {DoubleNum2} è: {DoubleProduct}");
+
+            // controllo le funzioni per i valori assoluti
+            int IntAbsoluteValue = CalculationsHelper.CalculateAbsoluteValue(IntNum3);
+            Console.WriteLine($"Il valore assoluto di {IntNum3} è: {IntAbsoluteValue}");
+
+            double DoubleAbsoluteValue = CalculationsHelper.CalculateAbsoluteValue(DoubleNum3);
+            Console.WriteLine($"Il valore assoluto di {DoubleNum3} è: {DoubleAbsoluteValue}");
+
+            // controllo le funzioni per il valore minimo
+            int IntMin = CalculationsHelper.CheckMinBetweenTwoNum(IntNum1, IntNum2);
+            Console.WriteLine($"Il minimo tra {IntNum1} e {IntNum2} è: {IntMin}");
+
+            double DoubleMin = CalculationsHelper.CheckMinBetweenTwoNum(DoubleNum1, DoubleNum2);
+            Console.WriteLine($"Il minimo tra {DoubleNum1} e {DoubleNum2} è: {DoubleMin}");
+
+            // controllo le funzioni per il valore massimo
+            int IntMax = CalculationsHelper.CheckMaxBetweenTwoNum(IntNum1, IntNum2);
+            Console.WriteLine($"Il massimo tra {IntNum1} e {IntNum2} è: {IntMax}");
+
+            double DoubleMax = CalculationsHelper.CheckMaxBetweenTwoNum(DoubleNum1, DoubleNum2);
+            Console.WriteLine($"Il massimo tra {DoubleNum1} e {DoubleNum2} è: {DoubleMax}");
+
+            // RISPOSTA ALLA DOMANDA: "Il fatto di dover scrivere lo stesso metodo per tipi di parametro diversi applica nella pratica uno dei principi di programmazione ad oggetti che abbiamo visto...Quale?"
+            // Applica nella pratica il principio del POLIMORFISMO, detto anche il terzo pilastro della programmazione orientata agli oggetti.
+            // Nel nostro caso specifico il polimorfismo viene applicato tramite l' "overloading" e permette l'utilizzo dello stesso metodo coerentemente ma per eseguire le operazioni su tipi di dato diversi.
+            // Grazie al polimorfismo il compilatore seleziona in automatico il giusto metodo
+
+            // bonus
+            double ExponentialNum = CalculationsHelper.CalculateExponentiation(IntNum1, IntNum3);
+            Console.WriteLine($"L'esponenziale di {IntNum1} con esponente {IntNum3} è: {ExponentialNum}");
         }
     }
 }
